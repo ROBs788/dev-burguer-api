a91ce5edabe70dd3264a336369c654d84f3879a2
ROBs788 <robsonarruda78820@gmail.com>
2026-08-28 19:36:36 -0300

    adicionando comentarios no UserController

--- src/app/models/controller/UserController.js
+++ src/app/models/controller/UserController.js
@@ -4,6 +4,7 @@ import * as Yup from 'yup';
 import bcrypt from 'bcrypt';
 
 /*
+//  UserController
 store -> criar dado
 index -> lista todos os dados
 show -> lista dados
@@ -13,28 +14,30 @@ delete -> remove dados
 */
 
 class UserController {
+
+    // configuração de formatação de dados //
     async store(request, response) {
         const schema = Yup.object({
             name: Yup.string().required(),
             email: Yup.string().email().required(),
             password: Yup.string().required().min(6),
             admin: Yup.boolean(),
-            
+
         })
 
-       // valida os dados recebidos // 
-try {
-    schema.validateSync(request.body, { abortEarly: false, strict: true });
-} catch (err) {
-    
-    return response.status(400).json({ error: err.errors });
-}
+        // valida os dados recebidos // 
+        try {
+            schema.validateSync(request.body, { abortEarly: false, strict: true });
+        } catch (err) {
 
-// pega os dados do corpo da requisição //
+            return response.status(400).json({ error: err.errors });
+        }
+
+        // pega os dados do corpo da requisição //
         const { name, email, password, admin } = request.body;
 
 
-// verifica se existe um usuário com o mesmo email //
+        // verifica se existe um usuário com o mesmo email //
         const existingUser = await User.findOne({
             where:
             {
@@ -45,10 +48,10 @@ try {
         //se existir um usuário com o mesmo email //
         if (existingUser) {
             return response
-            .status(400)
-            .json({ message: 'Email already taken!' });
+                .status(400)
+                .json({ message: 'Email already taken!' });
         }
-        
+
         // criptografa a senha do usuário //
         const password_hash = await bcrypt.hash(password, 10);
 
@@ -62,7 +65,7 @@ try {
 
         });
 
-// retorna os dados do usuário criado //
+        // retorna os dados do usuário criado //
         return response.status(201).json({
             id: user.id,
             name: user.name,
