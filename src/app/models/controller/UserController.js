6e7cbc2581ee6d19116481089f6c9d9ab0951791
ROBs788 <robsonarruda78820@gmail.com>
2026-08-28 19:23:44 -0300

    controller configurado, hash de senhas, err de crição de usuarios repetidos...

--- src/app/models/controller/UserController.js
+++ src/app/models/controller/UserController.js
@@ -1,6 +1,76 @@
+import { v4 } from 'uuid';
+import User from '../User.js';
+import * as Yup from 'yup';
+import bcrypt from 'bcrypt';
+
 /*
 store -> criar dado
 index -> lista todos os dados
+show -> lista dados
+update -> atualizar dados
+delete -> remove dados
 
 */
 
+class UserController {
+    async store(request, response) {
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().required().min(6),
+            admin: Yup.boolean(),
+            
+        })
+
+       // valida os dados recebidos // 
+try {
+    schema.validateSync(request.body, { abortEarly: false, strict: true });
+} catch (err) {
+    
+    return response.status(400).json({ error: err.errors });
+}
+
+// pega os dados do corpo da requisição //
+        const { name, email, password, admin } = request.body;
+
+
+// verifica se existe um usuário com o mesmo email //
+        const existingUser = await User.findOne({
+            where:
+            {
+                email
+            }
+        });
+
+        //se existir um usuário com o mesmo email //
+        if (existingUser) {
+            return response
+            .status(400)
+            .json({ message: 'Email already taken!' });
+        }
+        
+        // criptografa a senha do usuário //
+        const password_hash = await bcrypt.hash(password, 10);
+
+        // cria o usuário //
+        const user = await User.create({
+            id: v4(),
+            name,
+            email,
+            password_hash,
+            admin,
+
+        });
+
+// retorna os dados do usuário criado //
+        return response.status(201).json({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            admin: user.admin,
+        });
+    }
+}
+
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User.js';
-import { v4 as uuidv4 } from "uuid"
+import UserController from './app/models/controller/UserController.js';
+
 
 const routes = new Router();
 
-routes.get('/', async (_req, res) => {
- 
-  const user = {
-    id: uuidv4(),
-    name: 'Paulo',
-    email: 'n121K8T@example.com',
-    password_hash: '123456',
-    admin: false,
-  };
+routes.post('/users', UserController.store);
 
-  await User.create(user);
-
-  res.status(201).json(user);
-});
   export default routes;
